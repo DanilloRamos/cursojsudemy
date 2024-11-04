6d970d44b0c902d065c010889ada74059a14547c
DanilloR <danillo.ramos@dcomp.ufs.br>
2024-11-04 19:01:13 -0300

    Curso JS Udemy. Aulas e Exercícios

---
+++ 6_Mais Strings/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \n Segunda linha")
+console.log("'String entre aspas simples'")
+console.log('"String entre aspas duplas"')
+console.log(`A soma de 2+4 é igual a ${2+4}`)
+console.log("Concatenando"+" Strings")
---
+++ 8_Boolean Comparações/scripts.js
@@ -0,0 +1,7 @@
+console.log(1>2)
+console.log(5<10)
+console.log(3>=3)
+console.log(5<=4)
+console.log(2!=5)
+console.log(9===9)
+console.log(8==="8")
