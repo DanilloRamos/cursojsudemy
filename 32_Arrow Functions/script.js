46d9429d8dd76b0ee30e172903e113f9e9d8d1f5
DanilloRamos <danillosi__@live.com>
2024-11-07 16:57:08 -0300

    Definição de Funções em JS

---
+++ 32_Arrow Functions/script.js
@@ -0,0 +1,15 @@
+let consoleTeste = () => {
+    console.log("Olá mundo. Arrow Function")
+}
+
+consoleTeste();
+
+let soma = (a,b) => {
+    return a+b;
+}
+
+console.log(soma(10,20))
+
+const multiplicarPorDois = n => n*2
+
+console.log(multiplicarPorDois(4))
