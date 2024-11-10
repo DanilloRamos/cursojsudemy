8a11f3d45549889570eda1086c2bbf9a7583d463
DanilloRamos <danillosi__@live.com>
2024-11-10 15:40:48 -0300

    Arrays

---
+++ 37_Arrays/script.js
@@ -0,0 +1,9 @@
+let arr = [1,4,5,6,7]
+let nomes = ["João", "Maria","Josefa"]
+let bool = [true,false,true]
+
+let misturado = [1,"Danillo",true]
+
+console.log(arr)
+console.log(nomes)
+console.log(misturado)
