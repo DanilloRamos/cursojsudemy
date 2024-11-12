aa0e8f61abe47b919e47df5388ce946c02fb696c
DanilloRamos <danillosi__@live.com>
2024-11-12 17:11:44 -0300

    Aulas sobre métodos

---
+++ 63_Métodos/script.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    uivar:function() {
+        console.log("Auuuuuuuu")
+    },
+    rosnar:function(){
+        console.log('grrrrr')
+    }
+}
+
+cachorro.uivar()
+cachorro.rosnar()
