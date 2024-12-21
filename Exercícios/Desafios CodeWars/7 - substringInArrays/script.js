bbbdffc13d1ee009818a15dd9b3ad3c5f82420f7
DanilloRamos <danillo.ramos@dcomp.ufs.br>
2024-12-21 18:33:34 -0300

    Resolução de mais uma questão code wars

--- Exercícios/Desafios CodeWars/7 - substringInArrays/script.js
+++ Exercícios/Desafios CodeWars/7 - substringInArrays/script.js
@@ -1,11 +1,24 @@
 function inArray(array1,array2){
 
     let matches = []
-    let targets = array1
 
-    for(const word in array2){
-        const regex = new RegExp(word.toLowerCase(), 'g')
+    for(let i=0; i<array2.length; i++){
+        const subString = array1 [i] 
+        
+        for (let j=0; j<array2.length; j++){
+            const mainString = array2[j]
+
+            if (mainString.includes(subString)){
+                matches.push(subString)
+                break
+            }
+        }
     }
+
+    return array1.filter((item)=>matches.includes(item))
 }
 
-inArray()
+let a1 = ["arp", "live", "strong"]
+let a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+console.log(inArray(a1,a2))
