bbe3ea63cacbaba94cb0e6cdd385b60b45f99da9
DanilloRamos <danillosi__@live.com>
2025-01-14 10:47:00 -0300

    Simulação de deque utilizando duas pilhas - caderno de exercícios

---
+++ Exercícios/Caderno de Exercício Estruturas de Dados/Recursão/22 - umDequeDuasPilhas/pilha.js
@@ -0,0 +1,50 @@
+export class Pilha {
+    constructor(){
+        this.itens = {}
+        this.cont = 0
+    }
+
+    empilha(elemento){
+        this.itens[this.cont] = elemento
+        this.cont++
+    }
+
+    desempilha(){
+        if (this.vazia()) return undefined
+        
+        this.cont--
+        const removido = this.itens[this.cont]
+        delete this.itens[this.cont]
+
+        return removido
+    }
+
+    topo(){
+        return this.itens[this.cont-1]
+    }
+
+    vazia(){
+        return this.cont === 0
+    }
+
+    tamanho(){
+        return this.cont
+    }
+
+    esvazia(){  
+        this.itens = {}
+        this.cont = 0
+    }
+
+    imprimePilha(){
+        if (this.vazia()) return ''
+
+        let objString = `${this.itens[0]}`
+
+        for (let i=1; i<this.cont; i++){
+            objString = `${objString}, ${this.itens[i]}`
+        }
+
+        return objString
+    }
+}
---
+++ Exercícios/Caderno de Exercício Estruturas de Dados/Recursão/22 - umDequeDuasPilhas/script.js
@@ -0,0 +1,50 @@
+import { Pilha } from "./pilha.js";
+
+const pilha0 = new Pilha()
+const pilha1 = new Pilha()
+
+function adicionaNoInicio (elemento, numeroPilha){
+    
+    const origem = numeroPilha === 0 ? pilha0:pilha1
+    const destino = numeroPilha === 1 ? pilha1:pilha0
+
+    while (!origem.vazia()){
+        destino.empilha(origem.desempilha())
+    }
+
+    if (origem.vazia()){
+        origem.empilha(elemento)
+    }
+
+    while (!destino.vazia()){
+        origem.empilha(destino.desempilha())
+    }
+
+    console.log(`Pilha${numeroPilha}: ${origem.imprimePilha()}`)
+}
+
+function adicionaNoFinal(numeroPilha){
+    const origem = numeroPilha === 0 ? pilha0:pilha1
+    const destino = numeroPilha === 1 ? pilha1:pilha0
+
+    if (origem.vazia()){
+        console.log('A pilha de origem está vazia')
+        return
+    }
+
+    while(origem.tamanho()>1){
+        destino.empilha(origem.desempilha())
+    }
+
+    const removido = origem.desempilha()
+    console.log(`Elemento removido: ${removido}`)
+
+    while(!destino.vazia()){
+        origem.empilha(destino.desempilha())
+    }
+
+    console.log(`Pilha${numeroPilha}: ${origem.imprimePilha()}`)
+}
+
+adicionaNoInicio(5,0)
+adicionaNoInicio(12,0)
