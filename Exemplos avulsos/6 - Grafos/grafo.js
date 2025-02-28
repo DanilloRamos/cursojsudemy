b1c713937fd8dfbe7d57fc30c56cd59ee3b252d2
DanilloRamos <danillosi__@live.com>
2025-02-28 12:54:37 -0300

    implementação da busca em profundidade

--- Exemplos avulsos/6 - Grafos/grafo.js
+++ Exemplos avulsos/6 - Grafos/grafo.js
@@ -17,57 +17,78 @@ export class Grafo {
     }
 
     adicionaAresta(vertice,w){
-        this.adjList.get(vertice).push(w)
-        this.adjList.get(w).push(vertice)
+       if (!this.adjList.get(vertice)) this.adicionaVertice(vertice)       
+
+       if (!this.adjList.get(w)) this.adicionaVertice(w)
+
+       this.adjList.get(vertice).push(w)
+
+       if (!this.direcionado) this.adjList.get(w).push(vertice)
+
     }
 
-    inicializaCor(){
+    getVertices(){
+        return this.vertices
+    }
+
+    getAdjList(){
+        return this.adjList
+    }
+
+    Cores = {
+        BRANCO: 0,
+        CINZA: 1,
+        PRETO: 2
+    }
+
+    inicializaCor = function(){
         let cor = []
 
         for (let i=0; i<this.vertices.length; i++){
-            cor[this.vertices[i]] = 'branco'
+            cor[this.vertices[i]] = this.Cores.BRANCO
         }
 
         return cor
     }
 
-    buscaEmLargura(vertice, callback){
-        let cor = this.inicializaCor()
+    bfs = function(v, callback){
+        let cor = inicializaCor()
         let fila = new Fila()
 
-        fila.enfileirar(vertice)
+        fila.enfileirar(v)
 
         while(!fila.estaVazia()){
-            let u = fila.desenfileirar()
+            let u = fila.desenfileirar
             let vizinhos = this.adjList.get(u)
-            cor[u] = 'cinza'
+            cor[u] = this.Cores.CINZA
 
             for (let i=0; i<vizinhos.length; i++){
                 let w = vizinhos[i]
 
-                if (cor[w] === 'branco'){
-                    cor[w] = 'cinza'
+                if (cor[w] === this.Cores.BRANCO){
+                    cor[w] = this.Cores.CINZA
                     fila.enfileirar(w)
                 }
             }
-            cor[u] = 'preto'
+
+            cor[u] = this.Cores.PRETO
             callback(u)
         }
     }
 
-    imprimeNode(valor){
-        console.log('Vértice visitado: '+valor)
+    printNode(valor){
+        console.log('Vértice visitado: ' + valor)
     }
-
+    
     imprimeGrafo(){
         let s = ''
         let vizinhos
 
-        for (let i=0; j<this.vertices.length; i++){
+        for (let i=0; i<this.vertices.length; i++){
             s += this.vertices[i] + '->'
             vizinhos = this.adjList.get(this.vertices[i])
 
-            for (let j=0; i<vizinhos.length; j++){
+            for (let j=0; j<vizinhos.length; j++){
                 s += vizinhos[j] + ' '
             }
 
