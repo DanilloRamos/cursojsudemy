8dc0822a46c781eec1a47fc96671da0ff94d822c
DanilloRamos <danillosi__@live.com>
2025-03-13 11:04:08 -0300

    resolução do exercício de transposição de notas - codewars

--- Exercícios/Desafios CodeWars/18 - transposingASong/script.js
+++ Exercícios/Desafios CodeWars/18 - transposingASong/script.js
@@ -1,3 +1,33 @@
 function transpose(song, interval){
+    
+    let notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    let transp = []
   
+    for (let i=0; i<song.length; i++){
+
+        let note = song[i]
+
+        if (note === 'Bb') note = 'A#'
+        if (note === 'Db') note = 'C#'    
+        if (note === 'Eb') note = 'D#'
+        if (note === 'Gb') note = 'F#'
+        if (note === 'Ab') note = 'G#'
+
+        let index = notes.indexOf(note)
+
+        if (index === -1){
+            transp.push(note)
+            continue
+        }
+
+        let newIndex = (index+interval+notes.length)%notes.length
+        if (newIndex < 0) newIndex += notes.length
+
+        transp.push(notes[newIndex])
+  }
+
+  return transp
 }
+
+let song = ['A']
+console.log(transpose(song, 1))
