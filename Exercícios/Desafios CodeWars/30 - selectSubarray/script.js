c230a78ae633bc7a2528a05bb62fb57fd9f29770
DanilloRamos <danillosi__@live.com>
2025-03-21 16:28:53 -0300

    resolução de exercícios codewars

---
+++ Exercícios/Desafios CodeWars/30 - selectSubarray/script.js
@@ -0,0 +1,18 @@
+function selectSubarray(arr) { //Remove a Specific Element of an Array (Codewars)
+    if (arr.length <= 4) return false
+
+    function sumArray(arr){
+        return arr.reduce((sum, value) => sum + value, 0)
+    }
+
+    function multArray(arr){
+        return arr.reduce((mult, value) => mult * value, 1)
+    }
+
+    let subSum = 0
+    let subProduct = 1
+
+
+    
+    return []
+}
