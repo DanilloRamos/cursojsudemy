395322d40354d8207085199318e282e26ccca300
DanilloRamos <danillosi__@live.com>
2025-03-24 17:16:06 -0300

    alterações no exercício "remove a specific element of an array" do desafio codewars

--- Exercícios/Desafios CodeWars/30 - selectSubarray/script.js
+++ Exercícios/Desafios CodeWars/30 - selectSubarray/script.js
@@ -8,13 +8,34 @@ function selectSubarray(arr) { //Remove a Specific Element of an Array (Codewars
     function multArray(arr){
         return arr.reduce((mult, value) => mult * value, 1)
     }
+    
+    function minValue(arr){
+        return Math.min(...arr)
+    }
+    
+    let subArrays = []
+    let sums = []
+    let products = []
+    let qModule = []
+    let eliminateds = []
 
     for (let i=0; i<arr.length; i++){
         let sub = []
         for (let j=0; j<arr.length; j++){
             if (j !== i) sub.push(arr[j])
         }
+        
+        subArrays.push(sub)
+        sums.push(sumArray(sub))
+        products.push(multArray(sub))
+        
+        let eliminated = [i, arr[i]]
+        eliminateds.push(eliminated)
+        
+        let qValue = products[i]/sums[i]
+        if (qValue < 0) qValue *= -1
+        qModule.push(qValue)
     }
 
-    return q
+        return qModule
 }
