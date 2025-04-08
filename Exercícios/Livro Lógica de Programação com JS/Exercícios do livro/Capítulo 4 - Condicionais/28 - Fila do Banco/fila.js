306ca61fbf2550c4186c9359d3990b648e92dd8f
DanilloRamos <danillosi__@live.com>
2025-04-08 17:22:09 -0300

    resolução de exercícios

---
+++ Exercícios/Livro Lógica de Programação com JS/Exercícios do livro/Capítulo 4 - Condicionais/28 - Fila do Banco/fila.js
@@ -0,0 +1,37 @@
+export default class Fila {
+    constructor(){
+        this.cont = 0
+        this.itens = {}
+    }
+
+    enfileirar(item){
+        this.itens[this.cont] = item
+        this.cont++
+    }
+
+    desenfileirar(){ 
+        if (this.estaVazia()) return undefined
+
+        const saiu = this.itens[0]
+
+        for (let i=0; i<this.cont; i++) this.itens[i] = this.itens[i+1]
+
+        this.cont--
+
+        return saiu
+    }
+
+    estaVazia(){
+        return this.cont === 0 ? true:false
+    }
+
+    imprimeFila(){
+        if (this.estaVazia()) return ''
+
+        let objString = `${this.itens[0]}`
+
+        for (let i=1; i<this.cont; i++) objString += `${objString}, ${this.itens[i]}`
+
+        return objString
+    }
+}
