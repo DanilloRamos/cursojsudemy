274663e8b8ff01111c5ea9d54768226ab91616d9
DanilloRamos <danillosi__@live.com>
2025-05-06 16:51:36 -0300

    modificação e melhoramento do programa cadastro de vinhos

--- Exercícios/Livro Lógica de Programação com JS/Exercícios do livro/Capítulo 4 - Condicionais/50 - Cadastro de Vinhos/script.js
+++ Exercícios/Livro Lógica de Programação com JS/Exercícios do livro/Capítulo 4 - Condicionais/50 - Cadastro de Vinhos/script.js
@@ -1,73 +1,95 @@
 const frm = document.querySelector('form')
-const resp = document.querySelector('pre')
+const resp1 = document.querySelector('#pre1')
+const resp2 = document.querySelector('#pre2')
+const h1 = document.getElementById('resp1')
+const h2 = document.getElementById('resp2')
 
 const vinhos = []
-const lista = ``
 
 document.getElementById('inOpcoes').addEventListener('change', function() {
     const selecionado = this.value
+
     const p1 = document.getElementById('p1')
     const p2 = document.getElementById('p2')
     const p3 = document.getElementById('p3')
+    const p4 = document.getElementById('p4')
+    const p5 = document.getElementById('p5')
 
-    if (selecionado === 'incluir'){
-        p1.classList.remove('oculta')
-        p2.classList.remove('oculta')
-        p3.classList.remove('oculta')
-        p4.classList.remove('oculta')
-    } else {
-        p1.classList.add('oculta')
-        p2.classList.add('oculta')
-        p3.classList.add('oculta')
-        p4.classList.add('oculta')
+    //reseta todos os elementos
+    p1.classList.add('oculta')
+    p2.classList.add('oculta')
+    p3.classList.add('oculta')
+    p4.classList.add('oculta')
+    p5.classList.add('oculta')
+    resp1.classList.add('oculta')
+    resp2.classList.add('oculta')
+    document.getElementById('resp1').classList.add('oculta')
+    document.getElementById('resp2').classList.add('oculta')
+
+    switch(selecionado) {
+        case 'incluir':
+            p1.classList.remove('oculta')
+            p2.classList.remove('oculta')
+            p3.classList.remove('oculta')
+            p4.classList.remove('oculta')
+            break
+        
+        case 'listar':
+            h1.classList.remove('oculta')
+            resp1.classList.remove('oculta')
+
+            if (vinhos.length === 0) {
+                resp1.innerText = 'Não há vinhos cadastrados'
+            } else {
+                resp1.innerText = vinhos.map(vinho => 
+                    `${vinho.marca} | ${vinho.tipo} | R$ ${vinho.preco.toFixed(2)}`
+                ).join('\n')
+            }
+            break
+
+        case 'pesquisar':
+            h2.classList.remove('oculta')
+            resp2.classList.remove('oculta')
+            p5.classList.remove('oculta')
+            break
     }
-
 })
 
-
-frm.addEventListener('submit', (e) =>{
+frm.addEventListener('submit', (e) => {
     
     e.preventDefault()
-
+    
     const marca = frm.inMarca.value
     const tipo = frm.inTipo.value
-    const valor = Number(frm.inValor.value)
+    const preco = Number(frm.inValor.value)
 
-    vinhos.push({marca, tipo, valor})
+    vinhos.push({marca, tipo, preco})
     alert('Vinho cadastrado com sucesso!')
 
     frm.inMarca.value = ''
     frm.inTipo.value = ''
     frm.inValor.value = ''
 
-    resp.innerText = lista
+    frm.inMarca.focus()
 })
 
-document.getElementById('inOpcoes').addEventListener('change', function() {
-    const selecionado = this.value
+frm.btPesq.addEventListener('click', (e) => {
+    
+    e.preventDefault()
 
-    if (selecionado === 'listar'){
-        resp.classList.remove('oculta')
-        document.getElementById('resp1').classList.remove('oculta')
+    const procurado = frm.inTpPesq.value
+    
+    const encontrados = vinhos.filter(vinho => 
+        vinho.tipo.toUpperCase() === procurado.toUpperCase()
+    )
 
-        if (vinhos.length === 0) resp.innerText = 'Não há vinhos cadastrados!'
-
-        const lista = vinhos.map(vinho => 
-            `${vinho.marca} | ${vinho.tipo} | R$ ${vinho.valor.toFixed(2)}`
+    if (encontrados.length > 0){
+        resp2.innerText = encontrados.map(vinho =>
+            `${vinho.marca} | ${vinho.tipo} | R$ ${vinho.preco.toFixed(2)}`
         ).join('\n')
-
-        resp.textContent = lista
     } else {
-        resp.classList.add('oculta')
-        document.getElementById('resp1').classList.add('oculta')
+        resp2.innerText = `Não há vinhos do tipo ${procurado} cadastrado`
     }
 
-})
 
-document.getElementById('inOpcoes').addEventListener('change', function() {
-    const selecionado = this.value
-
-    if (selecionado === 'pesquisar'){
-        
-    }
 })
