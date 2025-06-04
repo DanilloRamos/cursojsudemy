afd622a1bca9797756fb46262eb85c00b9bc7c08
DanilloRamos <danillosi__@live.com>
2025-06-04 19:49:26 +0000

    reformulação do projeto job finder

--- Projetos/2 - Job Finder/app.js
+++ Projetos/2 - Job Finder/app.js
@@ -1,8 +1,33 @@
 const express = require ('express')
 const app = express()
+const db = require('./db/connection')
+const bodyParser = require('body-parser')
 
 const PORT = 3000
 
+
 app.listen(PORT, function() {
-    
+    console.log(`O express está rodando na porta ${PORT}`)
 })
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+//db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log('Conectou ao banco do sucesso')
+    })
+    .catch(err => {
+        console.log('Ocorreu um erro ao conectar', err)
+    })
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Está funcionando 2')
+})
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'))
