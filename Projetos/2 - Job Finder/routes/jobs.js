7e55f4dc0f7281cceeecc718f2af3b313ab7c416
DanilloRamos <danillosi__@live.com>
2025-06-06 19:47:22 +0000

    conclusão do projeto job finder

--- Projetos/2 - Job Finder/routes/jobs.js
+++ Projetos/2 - Job Finder/routes/jobs.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
+const { where } = require('sequelize')
 
 router.get('/test', (req, res) => {
     res.send('deu certo')
+})
+
+//detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne ({
+    where: {id: req.params.id}
+}).then(job => {
+    res.render('view', {
+        job
+    })
+}).catch(err => console.log(err))
+)
+
+//form da rota de envio
+router.get('/add', (req, res) => {
+    res.render('add')
 })
 
 //add job via post
